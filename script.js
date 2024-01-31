9434e1b69543c79b625408080bf42e8aeb280efa
JD-janydaniel <janydaniel59@gmail.com>
2024-01-31 12:10:34 +0530

    Day04 task uploaded

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1.Do the below programs in anonymous function & IIFE
+// a.Print odd numbers in an array
+// Solution
+// By using anonymous function
+var odd =[];
+var res = function(arr){
+    for( var i=0;i<arr.length;i++)
+    if(res(arr[i]%2!=0))
+    odd.push(arr[i]);
+return odd
+
+}
+console.log(res([1,2,3,4,5,6,7,8,9,10,11,12,13,14]));
